fa640162b6ba06ea8ea66289d27a3eb3f8db9298
xyyojl <m13631897256@163.com>
2019-02-28 17:50:19 +0800

    整理代码，完善README.md

--- js/main.js
+++ js/main.js
@@ -1,36 +1,28 @@
-/*
-思路：
-    1. 分析页面，使用什么数据结构
-    2. 使用JS自动生成标签，不用手动写，生成三个div，每个div里面有对应的个数的kbd
-    3. 可以假设kbd是一样，然后将固定的个数修改对应的多少个
-    4. 使用css美化界面
-    5. 封装函数，优化代码
-
-*/
 //1.初始化数据
-var hashA = init();
-var keys = hashA['keys'];
-var hash = hashA['hash'];
+let hashA = init();
+let keys = hashA['keys'];
+let hash = hashA['hash'];
 
 //2.生成键盘
 generateKeyBoard(keys,hash);
-
-
+        
+        
 //3.监听用户动作
 //要监听谁的键盘事件 main还是document，需要知道用户按的是什么键
-//key的兼容性不是很好
 listenToUser(hash);
 switchSearchEngin();
 
+
+
 // 下面是工具函数
 function init(){
-    var keys = {
+    let keys = {
         0 : ['q','w','e','r','t','y','u','i','o','p'],
         1 : ['a','s','d','f','g','h','j','k','l'],
         2 : ['z','x','c','v','b','n','m'],
         length : 3
     }
-    var hash = {
+    let hash = {
         q : 'qq.com',
         w : 'wangdoc.com',
         e : undefined,
@@ -46,7 +38,7 @@ function init(){
         f : undefined,
         g : 'github.com',
         h : undefined,
-        j : 'jianshu.com',
+        j : 'juejin.im',
         k : 'ke.qq.com',
         l : undefined,
         z : 'zhihu.com',
@@ -57,52 +49,49 @@ function init(){
         n : undefined,
         m : 'mail.163.com'
     }
-	//取出localStorage里面的zzz对应的hash 
-    var hasInLocalStorage = getFromLocalStorage('zzz');
+    let hasInLocalStorage = getFormLocalStorage('data');
     if(hasInLocalStorage){
         hash = hasInLocalStorage;
     }
-    return {
-        'keys' : keys,
-        'hash' : hash
-    }
+    return {'keys':keys,'hash':hash}
 }
 
-function generateKeyBoard(keys,hash){
-    for(var index = 0;index < keys['length'];index = index + 1){//0 1 2
-        var div = tag('div');
-        div.className = 'row';
+function getFormLocalStorage(name){
+    return JSON.parse(localStorage.getItem(name) || 'null')
+}
 
+function generateKeyBoard(keys, hash) {
+    for (let index = 0; index < keys['length']; index = index + 1) { //0 1 2
+        let div = tag('div');
         main.appendChild(div);
 
-        var row = keys[index];
-        for(var index2 = 0;index2 < row['length'];index2=index2+1){//将固定的10换成可控制的
-            var span = createSpan(row[index2]);
-
-            var img = createImage(hash[row[index2]]);
-
-            var button = createButton(row[index2]);
-
-            var kbd = tag('kbd');
-            var kbd_wrapper = tag('div');
+        let row = keys[index];
+        for (let index2 = 0; index2 < row['length']; index2 = index2 + 1) { //将固定的10换成可控制的
+            let kbd_wrapper = tag('div');
+            kbd_wrapper.className = 'kbd_wrapper';
+            let kbd = tag('kbd');
             kbd.className = 'key';
-
-            if(hash[row[index2]] === undefined){
-                kbd.setAttribute('title','未设置网站导航');
-            }else{
-                kbd.setAttribute('title',hash[row[index2]]);
+            let span = createSpan(row[index2]);
+            let img = createImage(hash[row[index2]]);
+            let button = createButton(row[index2]);
+        
+            // 判断按键是否已经有对应的网址
+            if (hash[row[index2]] === undefined) {
+                kbd.setAttribute('title', '未设置网站导航');
+            } else {
+                kbd.setAttribute('title', hash[row[index2]]);
             }
 
-            kbd.onclick = function(e){
-                var website = e.currentTarget.getAttribute('title');
+            kbd.onclick = function (e) {
+                let website = e.currentTarget.getAttribute('title');
                 if (website === '未设置网站导航') {
                     alert('请编辑此按键的网站再跳转')
                 } else {
+                    console.log(1)
                     window.open('http://' + website, "_blank");
                 }
             }
-            kbd_wrapper.className = 'kbd_wrapper';
-
+            
             kbd.appendChild(span);
             kbd.appendChild(img);
             kbd.appendChild(button);
@@ -111,123 +100,125 @@ function generateKeyBoard(keys,hash){
         }
     }
 }
-function listenToUser(hash){
-    var ifInputting = false;
-    var inputBar = document.getElementById('inputBar');
-    var searchBtn = document.querySelector('.searchBtn');
-    inputBar.addEventListener('focus',function(e){
-        ifInputting = true;
-        e.target.placeholder = '';    
-    })
-    inputBar.addEventListener('focusout',function(e){
-        ifInputting = false;
-        e.target.placeholder = '点击左边图标切换搜索引擎';
-    })
-    searchBtn.onclick = function(e){
-        e.preventDefault();
-        var searchContent = inputBar.value;
-        // 判断是什么搜索引擎
-        var searchEnginLogo = document.getElementById('searchEnginLogo');
-        var engin = searchEnginLogo.getAttribute('data-engin');
-        switch (engin) {
-            case 'baidu':
-                window.open("https://www.baidu.com/s?wd="+searchContent,'_blank');
-                break;
-            case 'google':
-                window.open("https://www.google.com/search?q="+searchContent,'_blank');
-                break;
-        }
-    }
 
-    document.onkeypress = function(xyz){
-        var key = xyz['key'];
-        var website = hash[key];
-        // && website !== undefined
-        if(!ifInputting){
-            if (website === undefined) {
-                alert('请编辑此按键的网站再跳转')
-            } else {
-                // window.open('http://' + website, "_blank");
-                setTimeout(function(){
-                    window.open('http://'+website,'_blank')	//新窗口打开网页	
-                },500)
-            }
-        }
-    }
-}
-function getFromLocalStorage(name){
-    return JSON.parse(localStorage.getItem(name) || 'null');
-}
-function tag(tagName){
-    var element = document.createElement(tagName);
+function tag(tagName) {
+    let element = document.createElement(tagName);
     return element;
 }
-function createSpan(textContent){
-    var span = tag('span');
-    span.textContent = textContent;//第一个数组 第二个数组 第三个数组
+
+function createSpan(textContent) {
+    let span = tag('span');
+    span.textContent = textContent; //第一个数组 第二个数组 第三个数组
     span.className = 'text';
     return span;
 }
-function createButton(id){
-    var button = tag('button');
-        button.textContent = 'e';
-        button.id = id;
-        button.onclick = function(e){
-            //阻止事件冒泡
-            e.stopPropagation();
-            var button2 = e['target'];
-            var img2 = button2.previousSibling;
-            //获取当前的id
-            var key = button2['id'];
-            //用户输入一个网址
-            var web = prompt('请输入一个网址：');
-            //将原来的hash给替换掉
-            hash[key] = web;
-            img2.src = 'http://' + web + '/favicon.ico';
+
+function createButton(id) {
+    let button = tag('button');
+    button.textContent = 'e';
+    button.id = id;
+    button.onclick = function (e) {
+        //阻止事件冒泡
+        e.stopPropagation();
+        let button2 = e['target'];
+        let img2 = button2.previousSibling;
+        //获取当前的id
+        let key = button2['id'];
+        //用户输入一个网址
+        let web = prompt('请输入一个网址：');
+        //将原来的hash给替换掉
+        hash[key] = web;
+        img2.src = 'http://' + web + '/favicon.ico';
+        img2.onerror = function (e) {
             e.target.src = './image/dot.png';
-            localStorage.setItem('zzz',JSON.stringify(hash));
         }
-    
+        localStorage.setItem('data', JSON.stringify(hash));
+    }
     return button;
 }
-function createImage(domain){//hash[row[index2]]
-    var img = tag('img');
-    if(domain){
+
+function createImage(domain) { //hash[row[index2]]
+    let img = tag('img');
+    if (domain) {
         img.src = 'http://' + domain + '/favicon.ico';
-    }else{
+    } else {
         img.src = './image/dot.png';
     }
-    img.onerror = function(xyz){
-        xyz.target.src = './image/dot.png';
+    img.onerror = function (e) {
+        e.target.src = './image/dot.png';
     }
     return img;
 }
 
+
+function listenToUser(hash) {
+    // ifInputting作为一个开关
+    let ifInputting = false;
+    let inputBar = document.getElementById('inputBar');
+    let searchBtn = document.querySelector('.searchBtn');
+    inputBar.addEventListener('focus', function (e) {
+        ifInputting = true;
+        e.target.placeholder = '';
+    })
+    inputBar.addEventListener('focusout', function (e) {
+        ifInputting = false;
+        e.target.placeholder = '点击左边图标切换搜索引擎';
+    })
+    searchBtn.onclick = function (e) {
+        e.preventDefault();
+        let searchContent = inputBar.value;
+        // 判断是什么搜索引擎
+        let searchEnginLogo = document.getElementById('searchEnginLogo');
+        let engin = searchEnginLogo.getAttribute('data-engin');
+        switch (engin) {
+            case 'baidu':
+                window.open("https://www.baidu.com/s?wd=" + searchContent, '_blank');
+                break;
+            case 'google':
+                window.open("https://www.google.com.hk/search?q=" + searchContent, '_blank');
+                break;
+        }
+    }
+
+    document.onkeypress = function (e) {
+        let key = e['key'];
+        let website = hash[key];
+        if (!ifInputting) {
+            if (website === undefined) {
+                alert('请编辑此按键的网站再跳转')
+            } else {
+                console.log(1);
+                window.open('http://' + website, "_blank");
+            }
+        }
+    }
+}
+
 // 切换搜索引擎
 function switchSearchEngin() {
-    var ifSwitch = false;
-    // false代表要切换成引擎google，true代表要切换成引擎baidu
-    var searchEnginLogo = document.getElementById('searchEnginLogo');
-    var baiduLogo = document.querySelector('#searchEnginLogo li:nth-child(1)');
-    var googleLogo = document.querySelector('#searchEnginLogo li:nth-child(2)');
-    var baiduPic = document.querySelector('#searchEnginPic li:nth-child(1)');
-    var googlePic = document.querySelector('#searchEnginPic li:nth-child(2)');
-    searchEnginLogo.setAttribute('data-engin','baidu');
-    searchEnginLogo.onclick = function(){
+    // 搜索引擎默认是google
+    let ifSwitch = false;
+    let searchEnginLogo = document.getElementById('searchEnginLogo');
+    let googleLogo = document.querySelector('#searchEnginLogo li:nth-child(1)');
+    let baiduLogo = document.querySelector('#searchEnginLogo li:nth-child(2)');
+    let googlePic = document.querySelector('#searchEnginPic li:nth-child(1)');
+    let baiduPic = document.querySelector('#searchEnginPic li:nth-child(2)');
+    searchEnginLogo.setAttribute('data-engin', 'google');
+    searchEnginLogo.onclick = function () {
         if (!ifSwitch) {
-            // baidu --> google
-            baiduLogo.classList.remove('active');
-            googleLogo.classList.add('active');
-            baiduPic.classList.remove('active');
-            googlePic.classList.add('active');
-            searchEnginLogo.setAttribute('data-engin','google');
-        }else{
             // google --> baidu
             googleLogo.classList.remove('active');
             baiduLogo.classList.add('active');
             googlePic.classList.remove('active');
             baiduPic.classList.add('active');
-            searchEnginLogo.setAttribute('data-engin','baidu');
+            searchEnginLogo.setAttribute('data-engin', 'baidu');
+        } else {
+            // baidu --> google
+            baiduLogo.classList.remove('active');
+            googleLogo.classList.add('active');
+            baiduPic.classList.remove('active');
+            googlePic.classList.add('active');
+            searchEnginLogo.setAttribute('data-engin', 'google');
         }
         ifSwitch = !ifSwitch;
     }
